e0ece4048c4cf79b1806d94c4646af9a0203b04d
Olivier NEUGNOT <oliviergenilac@gmail.com>
2025-02-21 16:00:22 +0100

    feat(add): commit
    new pop up add

--- app.js
+++ app.js
@@ -1,13 +1,88 @@
-const game = [
-	[0, 0, 0, 0, 0, 1],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
-	[0, 0, 0, 0, 0, 0],
+const grid = [
+	["x", "x", "x", "x", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
+	["", "", "", "", "", "", ""],
 ];
+let winningArray = [
+	[0, 1, 2, 3],
+	[41, 40, 39, 38],
+	[7, 8, 9, 10],
+	[34, 33, 32, 31],
+	[14, 15, 16, 17],
+	[27, 26, 25, 24],
+	[21, 22, 23, 24],
+	[20, 19, 18, 17],
+	[28, 29, 30, 31],
+	[13, 12, 11, 10],
+	[35, 36, 37, 38],
+	[6, 5, 4, 3],
+	[0, 7, 14, 21],
+	[41, 34, 27, 20],
+	[1, 8, 15, 22],
+	[40, 33, 26, 19],
+	[2, 9, 16, 23],
+	[39, 32, 25, 18],
+	[3, 10, 17, 24],
+	[38, 31, 24, 17],
+	[4, 11, 18, 25],
+	[37, 30, 23, 16],
+	[5, 12, 19, 26],
+	[36, 29, 22, 15],
+	[6, 13, 20, 27],
+	[35, 28, 21, 14],
+	[0, 8, 16, 24],
+	[41, 33, 25, 17],
+	[7, 15, 23, 31],
+	[34, 26, 18, 10],
+	[14, 22, 30, 38],
+	[27, 19, 11, 3],
+	[35, 29, 23, 17],
+	[6, 12, 18, 24],
+	[28, 22, 16, 10],
+	[13, 19, 25, 31],
+	[21, 15, 9, 3],
+	[20, 26, 32, 38],
+	[36, 30, 24, 18],
+	[5, 11, 17, 23],
+	[37, 31, 25, 19],
+	[4, 10, 16, 22],
+	[2, 10, 18, 26],
+	[39, 31, 23, 15],
+	[1, 9, 17, 25],
+	[40, 32, 24, 16],
+	[9, 7, 25, 33],
+	[8, 16, 24, 32],
+	[11, 7, 23, 29],
+	[12, 18, 24, 30],
+	[1, 2, 3, 4],
+	[5, 4, 3, 2],
+	[8, 9, 10, 11],
+	[12, 11, 10, 9],
+	[15, 16, 17, 18],
+	[19, 18, 17, 16],
+	[22, 23, 24, 25],
+	[26, 25, 24, 23],
+	[29, 30, 31, 32],
+	[33, 32, 31, 30],
+	[36, 37, 38, 39],
+	[40, 39, 38, 37],
+	[7, 14, 21, 28],
+	[8, 15, 22, 29],
+	[9, 16, 23, 30],
+	[10, 17, 24, 31],
+	[11, 18, 25, 32],
+	[12, 19, 26, 33],
+	[13, 20, 27, 34],
+];
+const gridCase = document.querySelector(".grid-btn");
 
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let chronometre = 0;
+let tourPlayer = "";
 const grilleColumns = document.querySelectorAll(".grid-col");
 const backgroundChangeWin = document.querySelector(".background-raduis");
 const ruleBtn = document.querySelector(".rules-btn");
@@ -15,7 +90,7 @@ const imgFourGame1 = "./assets/player1.svg";
 const imgFourGame2 = "./assets/player2.svg";
 const playerVsPlayer = document.querySelector(".playervs");
 const menuPrinc = document.querySelector(".first-menu");
-const btnMenu = document.querySelector(".menu-back");
+const btnMenu = document.querySelector(".nav-button");
 const body = document.body;
 
 console.log(grilleColumns);
@@ -93,17 +168,78 @@ function menuPause() {
 	const fullMenu = document.createElement("div");
 	const pauseMenu = document.createElement("div");
 	const spanPause = document.createElement("span");
+	const divBtnPause = document.createElement("div");
+	const btnPause1 = document.createElement("button");
+	const btnPause2 = document.createElement("button");
+	const btnPause3 = document.createElement("button");
+
+	fullMenu.classList.add("full-menu");
+	pauseMenu.classList.add("pause");
+	spanPause.classList.add("text-pause");
+	spanPause.classList.add("pause-white");
+	divBtnPause.classList.add("div-btn-pause");
+	btnPause1.classList.add("btn-pause");
+	btnPause2.classList.add("btn-pause");
+	btnPause3.classList.add("btn-pause");
+	btnPause3.classList.add("pink-btn");
+
+	spanPause.textContent = "PAUSE";
+	btnPause1.textContent = "CONTINUE-GAME";
+	btnPause2.textContent = "RESTART";
+	btnPause3.textContent = "QUIT GAME";
+
+	pauseMenu.appendChild(spanPause);
+	divBtnPause.appendChild(btnPause1);
+	divBtnPause.appendChild(btnPause2);
+	divBtnPause.appendChild(btnPause3);
+	fullMenu.appendChild(pauseMenu);
+	pauseMenu.appendChild(divBtnPause);
+	body.appendChild(fullMenu);
+
+	btnPause1.addEventListener("click", () => {
+		fullMenu.remove();
+	});
+
+	btnPause3.addEventListener("click", () => {
+		body.appendChild(menuPrinc);
+		fullMenu.remove();
+	});
+	return;
 }
 
 ruleBtn.addEventListener("click", activeRulesBtn);
 
-btnMenu.addEventListener("click", () => {});
+btnMenu.addEventListener("click", () => {
+	menuPause();
+});
 
-for (let i = 0; i < grilleColumns.length; i++) {
-	const grilleCol = grilleColumns[i];
-	const numCol = grilleCol.getAttribute("data-col");
+// for (let i = 0; i < grilleColumns.length; i++) {
+// 	const grilleCol = grilleColumns[i];
+// 	const numCol = grilleCol.getAttribute("data-col");
 
-	grilleCol.addEventListener("click", () => {
-		console.log(game[numCol]);
-	});
+// 	grilleCol.addEventListener("click", () => {
+// 		console.log(game[numCol]);
+// 	});
+// }
+
+function winningCheck(gridcheck) {
+	for (let i = 0; i < winningArray.length; i++) {
+		const element = winningArray[i];
+		for (let j = 0; j < element.length; j++) {
+			const elementcheck = element[j];
+			for (let h = 0; h < grid.length; h++) {
+				const gridelement = grid[h];
+				for (let a = 0; a < gridelement.length; a++) {
+					const littleelementgrid = gridelement[a];
+					if (littleelementgrid == elementcheck) {
+						console.log("true");
+					}
+				}
+			}
+		}
+	}
 }
+const btnSelect = document.querySelector(".btn-grid");
+btnSelect.addEventListener("click", () => {
+
+});
